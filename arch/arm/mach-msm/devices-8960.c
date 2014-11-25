99f3f0dd68379bdb5db1d596f6c4e733f1c3272d
Matt Mower <mowerm@gmail.com>
2014-11-24 19:41:33 -0600

    msm8960: Restrict vcodec freq 266670000 to m4
    Change-Id: I257bbe5ddc48227d9b871ab597d51b3bfcb0ee40

--- arch/arm/mach-msm/devices-8960.c
+++ arch/arm/mach-msm/devices-8960.c
@@ -1198,6 +1198,9 @@ struct msm_vidc_platform_data vidc_platform_data = {
 #endif
 	.disable_dmx = 0,
 	.disable_fullhd = 0,
+#ifndef CONFIG_MACH_M4_UL
+	.disable_turbo = 1,
+#endif
 	.cont_mode_dpb_count = 18,
 	.fw_addr = 0x9fe00000,
 	.enable_sec_metadata = 0,
