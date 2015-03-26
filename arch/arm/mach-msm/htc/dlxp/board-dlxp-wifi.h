e727c2d60225481ef7dacb4e499a3015baf1c228
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-03-27 07:19:25 +1030

    msm: HTC: DLXP: Import DLXP_UL board files
    * Add support for HTC Butterfly S
    * Yeah it's broken - very broken.. This is just to hold it.
    * This needs a cleanup too as it's based on m7.
    * Display won't turn on yet but I have it working, just need to clean it up later. .
    * Probably doesn't even compile right now, it will be fixed though. Soon. I swear.
    
    Change-Id: Ia87de7806e7a8a567ea4f26cf8f01be1474a1b24

---
+++ arch/arm/mach-msm/htc/dlxp/board-dlxp-wifi.h
@@ -0,0 +1,18 @@
+/* linux/arch/arm/mach-msm/board-dlxp-wifi.h
+ *
+ * Copyright (C) 2008 HTC Corporation.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+extern unsigned char *get_wifi_nvs_ram(void);
+extern int dlxp_wifi_power(int on);
+extern int dlxp_wifi_reset(int on);
+extern int dlxp_wifi_set_carddetect(int on);
