3abdc0805ff97abd2f00bb03de058c3cd4b405e2
Ethan Chen <intervigil@gmail.com>
2015-04-24 23:56:39 -0700

    msm: mdp: Add support for display always on mode
    * Add a new panel power state to represent a panel low power mode.
    
    Change-Id: Ie307012ddd62f9690b99b44c3efd9cac6895beb1

--- drivers/video/msm/msm_fb_panel.h
+++ drivers/video/msm/msm_fb_panel.h
@@ -214,6 +214,7 @@ struct msm_fb_panel_data {
 	int (*clk_func) (int enable);
 	int (*fps_level_change) (struct platform_device *pdev,
 					u32 fps_level);
+	int (*low_power_config) (struct platform_device *pdev, int enable);
 #ifdef CONFIG_CABC_DIMMING_SWITCH
 	void (*dimming_on) (struct msm_fb_data_type *);
 #endif
@@ -231,6 +232,7 @@ int panel_next_on(struct platform_device *pdev);
 int panel_next_off(struct platform_device *pdev);
 int panel_next_fps_level_change(struct platform_device *pdev,
 					u32 fps_level);
+int panel_next_low_power_config(struct platform_device *pdev, int enable);
 int panel_next_late_init(struct platform_device *pdev);
 int panel_next_early_off(struct platform_device *pdev);
 
