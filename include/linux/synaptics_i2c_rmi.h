8d6616717a6435a69423c4072d691ad3c7538442
flar2 <asegaert@gmail.com>
2015-07-28 21:13:29 +0300

    S2W/DT2W/L2M/L2W
    Change-Id: Ib8e521eb2da606bf47043dd5850a32bf5c4c88d6
    Signed-off-by: flar2 <asegaert@gmail.com>

--- include/linux/synaptics_i2c_rmi.h
+++ include/linux/synaptics_i2c_rmi.h
@@ -17,6 +17,10 @@
 #ifndef _LINUX_SYNAPTICS_I2C_RMI_H
 #define _LINUX_SYNAPTICS_I2C_RMI_H
 
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
+#include <linux/input.h>
+#endif
+
 #define SYNAPTICS_I2C_RMI_NAME "synaptics-rmi-ts"
 #define SYNAPTICS_T1007_NAME "synaptics-t1007"
 #define SYNAPTICS_T1021_NAME "synaptics-t1021"
@@ -185,6 +189,13 @@ enum {
 	FUNCTION
 };
 
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
+extern void sweep2wake_setdev(struct input_dev * input_device);
+#endif
+
+>>>>>>> 5623de1... S2W/DT2W/L2M/L2W
 extern uint8_t getPowerKeyState(void);
 #endif /* CONFIG_TOUCHSCREEN_SYNAPTICS_3K */
 #endif /* _LINUX_SYNAPTICS_I2C_RMI_H */
